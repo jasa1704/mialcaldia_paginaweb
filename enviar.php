06141049d5dad25a3714634df782b523dc4764b8
jasa1704 <jasosapanta@gmail.com>
2018-08-14 11:10:33 -0500

    Inicio proyecto mi alcaldia

---
+++ enviar.php
@@ -0,0 +1,23 @@
+<?php
+
+// Llamando a los campos 
+$nombre = $_POST['nombre'];
+$correo = $_POST['correo'];
+$telefono = $_POST['telefono'];
+$mensaje = $_POST['mensaje'];
+
+// Datos para el correo
+$destinatario = "jasosapanta@gmail.com";
+$asunto = "Correo enviado desde la pagina web mialcaldia";
+
+$carta = "De: $nombre \n";
+$carta .= "Correo: $correo \n";
+$carta .= "Telefono: $telefono \n";
+$carta .= "Mensaje: $mensaje \n";
+
+// Enviando Mensaje
+
+mail($destinatario, $asunto, $carta);
+header('Location:mensajeEnviado.html');
+
+?>
