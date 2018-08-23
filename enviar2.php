d15ec48ef2d555279764c795167429afdd17b034
jasa1704 <jasosapanta@gmail.com>
2018-08-23 10:35:07 -0500

    Proyecto mialcaldia finalizado

---
+++ enviar2.php
@@ -0,0 +1,20 @@
+<?php
+
+// Llamando a los campos 
+$correo = $_POST['correo'];
+
+// Datos para el correo
+$destinatario = "comercial@appsus.com";
+$asunto = "Mensaje envio desde la pagina principal";
+
+$carta = "De: Usuario pagina principal";
+$carta .= "Correo: $correo \n";
+$carta .= "Telefono: No se tiene";
+$carta .= "Mensaje: Quisiera saber mas sobre mi alcaldia";
+
+// Enviando Mensaje
+
+mail($destinatario, $asunto, $carta);
+header('Location:index.html');
+
+?>
